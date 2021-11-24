63b87051c2b4d0f87d6fd64470ebceac8ac57c77
Franco <frank.stockel@gmail.com>
2021-11-24 12:27:20 +0100

    Added icon in browser tab

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;600;700&display=swap">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="media/favicon.png">
     
 </head>
 <body>
