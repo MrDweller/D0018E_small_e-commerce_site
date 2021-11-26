6259d842eace4a7dc7be4142afa2bf2fd350c10e
MrDweller <jesper.frisk@hotmail.com>
2021-11-26 17:13:01 +0100

    user_settings

--- include/admin_functions.inc.php
+++ include/admin_functions.inc.php
@@ -1,8 +1,29 @@
 <?php
 
-    function get_all_users()
+    function get_all_users($conn)
     {
-        
+        $sql = "SELECT usersUID, usersEmail FROM users";
+
+        $result = mysqli_query($conn, $sql);
+
+        $resultCheck = mysqli_num_rows($result);
+
+        $data = array();
+        if($resultCheck > 0)
+        {
+            $i = 0;
+            while($row = mysqli_fetch_array($result))
+            {
+                $row_data = array();
+
+                $row_data[0] = $row[0];
+                $row_data[1] = $row[1];
+
+                $data[$i] = $row_data;
+                $i++;
+            }
+        }
+        return $data;
     }
 
     function is_admin($conn, $usersID)
--- user_settings.php
+++ user_settings.php
@@ -2,8 +2,26 @@
     require_once 'header.php';
     require_once 'admin_header.php';
 ?>
-
-
+    <table class="center">
+        <tr>
+            <th style="padding-right: 20px;">User</th>
+            <th style="padding-right: 150px;">Email</th>
+        </tr>
+        <?php
+            require_once 'include/admin_functions.inc.php';
+            $user_array = get_all_users($conn);
+            for($i = 0; $i < sizeof($user_array); $i++)
+            {
+                ?>
+                    <tr>
+                        <td class="user_column"><?php echo $user_array[$i][0] ?></td>
+                        <td class="user_column"><?php echo $user_array[$i][1] ?></td>
+                    </tr>
+                <?php
+            }
+        ?>
+    </table>
+    <br>
 
 <?php
     require_once 'footer.php';
